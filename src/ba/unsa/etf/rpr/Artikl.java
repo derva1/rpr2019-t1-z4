1a66be464d069eb2d196440a86a7915f0c498d18
derva1 <vdervisevi1@etf.unsa.ba>
2020-10-27 17:36:05 +0100

    Izmjene u klasi Artikl

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    String ime;
+    int cijena;
+    String kod;
+    public Artikl(String ime,int cijena,String kod){
+        this.ime=ime ; this.cijena=cijena; this.kod=kod;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public int getCijena() {
+        return cijena;
+    }
+
+    public String getKod() {
+        return kod;
+    }
 }
